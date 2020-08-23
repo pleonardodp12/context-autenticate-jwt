36909244dc76dbc458aeeec65209b4a2ff9ae27f
pleonardodp12 <pauloleonardo_dp@outlook.com>
2020-08-22 23:07:09 -0300

    loading, login data

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,38 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 import api from '../api';
+import history from '../history';
 
 const Context = createContext();
 
 function AuthProvider({ children }) {
   const [authenticated, setAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+
+      setAuthenticated(true);
+    }
+
+    setLoading(false);
+  }, [])
 
   async function handleLogin() {
-    const { data } = await api.post('/authenticate');
-    setAuthenticated(true)
+    const { data: { token } } = await api.post('/authenticate');
+
+
+    localStorage.setItem('token', JSON.stringify(token));
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+    history.push('/users');
+    setAuthenticated(true);
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>
   }
 
   return (
